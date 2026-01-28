e6714302551bb385e38659771ba6785694e93cee
christndjiya-dev <christndjiya@gmail.com>
2026-01-28 07:31:24 +0100

    feat : gestion des notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_ELEVES = 30;
 
 int main() {
     int nbEleves;
-
-const int MAX_ELEVES = 5;
     string noms[MAX_ELEVES];
+    float notes[MAX_ELEVES];
 
     cout << "Nombre d'eleves (max " << MAX_ELEVES << ") : ";
     cin >> nbEleves;
@@ -19,6 +19,16 @@ const int MAX_ELEVES = 5;
     for (int i = 0; i < nbEleves; i++) {
         cout << "Nom de l'eleve " << i + 1 << " : ";
         cin >> noms[i];
+    }
+
+    for (int i = 0; i < nbEleves; i++) {
+        cout << "Note de " << noms[i] << " : ";
+        cin >> notes[i];
+
+        while (notes[i] < 0 || notes[i] > 20) {
+            cout << "Note invalide. Reessayez : ";
+            cin >> notes[i];
+        }
     }
 
     return 0;
